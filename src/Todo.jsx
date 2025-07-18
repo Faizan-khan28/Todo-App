48a6c2c4dae9dfa047507347cff98a8ca8e85469
faizanKhan <faizankhan46849@gmail.com>
2025-07-18 21:53:59 +0530

    todo practice

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,55 +1,41 @@
-import Button from '@mui/material/Button';
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function Todolist () {
+export default function Todolist() {
+  const [todo, setTodo] = useState([]);
+  const [task, setTask] = useState("");
 
-    let [todos, setTodos] = useState(["sample task"])
-    let [newTodo, setnewTodo] = useState("")
+  let handleChange = event => {
+    setTask(event.target.value);
+  };
 
-    let handleChange = (event) => {
-       setnewTodo(event.target.value) 
-    }
+  let addNewTodo = () => {
+    setTodo([...todo, task]);
+    setTask("");
+  };
 
-    let addnewTask = ()=> {
-        setTodos([...todos, newTodo])
-        setnewTodo("");
-    }
-    
-    let deleteTodo = (todo) => {
-       setTodos( todos.filter((task) => task != todo))
-    }
-
-    let uppercase = () => {
-        setTodos(todos.map((todos) => todos.toUpperCase()));
-    }
-
-   let lowercase = () => {
-    setTodos(todos.map( (todo) => todo.toLowerCase()))
-   }
-
-   
-
-    return (
-        <div className='flex flex-col'>
-            <h1 className=" flex justify-center w-[100vw] bg-black text-white font-medium text-[50px]">Todolist</h1>
-           <div className="flex items-center flex-col w-[100vw] mt-7">
-             <div className='flex items-center gap-3'>
-             <input onChange={handleChange} value={newTodo} className=' border-[2px] border-black w-[15rem] h-[35px] pl-2' type='text' placeholder='Enter Task'></input>
-             <Button onClick={addnewTask}  style={{background: "black", color: "white"}} variant="contained">Add Task</Button>
-             </div>
-
-             <ul className='mt-4'>
-                {
-                    todos.map( (todo) => (
-                        <li key={todo} className=' text-[25px]'><span>{todo}</span>
-                        <Button onClick={() => deleteTodo(todo)} style={{marginLeft: "15px", background: "black"}} variant="contained" >Delete Task</Button>
-                        <Button onClick={ uppercase} style={{marginLeft: "15px", background: "black"}} variant="contained" >UpperCase</Button>
-                        <Button onClick={ lowercase} style={{marginLeft: "15px", background: "black"}} variant="contained" >LowerCase</Button>
-                        </li>
-                    ))
-                }
-             </ul>
-           </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-semibold">todo app</h1>
+      <div className="flex justify-center gap-2">
+        <input
+          className="border w-56 pl-3 border-black"
+          type="text"
+          value={task}
+          onChange={handleChange}
+          placeholder="Enter task"
+        />
+        <button
+          onClick={addNewTodo}
+          className="bg-blue-400 px-3 font-semibold py-1"
+        >
+          Add task
+        </button>
+      </div>
+      <ul className="text-center mt-3 font-medium text-xl">
+        {todo.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
